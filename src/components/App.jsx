6aefca495cdc4d92fddc76b6b43c958239b3d9d9
Enikő Veszelka <encsooo@gmail.com>
2021-04-27 16:49:10 +0200

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <h1>app is here</h1>
+    </div>
+  );
+};
+
+export default App;
