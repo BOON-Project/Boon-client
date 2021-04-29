0c837832e28ba156ef4362587e1a2083719ce7c5
Azael Alonso Campana <zarzarius@gmail.com>
2021-04-29 21:30:15 +0200

    Navbar conditional rendering working with authenticated User

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,16 +9,23 @@ import Link from "@material-ui/core/Link";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import useStyles from "./styles";
+import { useSelector } from "react-redux";
 
 export default function PrimarySearchAppBar() {
-  const [isAuth, setIsAuth] = useState();
+  const user = useSelector((state) => state.authReducer.user);
+  const [isAuth, setIsAuth] = useState(false);
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
-
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
+  useEffect(() => {
+    if (Object.keys(user).length) setIsAuth(true);
+    console.log("====================================");
+    console.log(isAuth);
+    console.log("====================================");
+  }, []);
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -37,7 +44,7 @@ export default function PrimarySearchAppBar() {
   };
 
   const menuId = "primary-search-account-menu";
-  const renderMenu = (
+  const renderMenu = !isAuth ? (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
@@ -57,10 +64,50 @@ export default function PrimarySearchAppBar() {
         </Link>{" "}
       </MenuItem>
     </Menu>
+  ) : (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}>
+      <MenuItem onClick={handleMenuClose}>
+        <Link underline='none' href='/UserProfile' color='primary'>
+          Profile
+        </Link>{" "}
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Link underline='none' href='/Logout' color='primary'>
+          Logout
+        </Link>{" "}
+      </MenuItem>
+    </Menu>
   );
 
   const mobileMenuId = "primary-account-menu-mobile";
-  const renderMobileMenu = (
+  const renderMobileMenu = !isAuth ? (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label='account of current user'
+          aria-controls='primary-search-account-menu'
+          aria-haspopup='true'
+          color='inherit'>
+          <AccountCircle />
+        </IconButton>
+        <Link href='/UserProfile'>Profile</Link>
+      </MenuItem>
+    </Menu>
+  ) : (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
@@ -84,85 +131,138 @@ export default function PrimarySearchAppBar() {
           Explore
         </Link>
       </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label='account of current user'
-          aria-controls='primary-search-account-menu'
-          aria-haspopup='true'
-          color='inherit'>
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
+
+      <IconButton
+        aria-label='account of current user'
+        aria-controls='primary-search-account-menu'
+        aria-haspopup='true'
+        color='inherit'>
+        <AccountCircle />
+      </IconButton>
+      <Link href='/UserProfile'>Profile</Link>
     </Menu>
   );
-
-  return (
-    <div className={classes.grow}>
-      <AppBar className={classes.appBar} position='static'>
-        <Toolbar>
-          <Link underline='none' href='/' color='primary'>
-            <img
-              src='./images/Boon-big.svg'
-              alt='Boon-logo'
-              className={classes.logo}
-            />
+  if (!isAuth) {
+    return (
+      <div className={classes.grow}>
+        <AppBar className={classes.appBar} position='static'>
+          <Toolbar>
+            <Link underline='none' href='/' color='primary'>
+              <img
+                src='./images/Boon-big.svg'
+                alt='Boon-logo'
+                className={classes.logo}
+              />
+            </Link>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              {/* {user._id && (<Link
+            underline='none'
+            href='#'
+            className={classes.link}
+            color='inherit'>
+            Request a Boon
           </Link>
-          {/* <Typography
-            className={classes.title}
-            color='primary'
-            variant='h3'
-            noWrap>
-            Boon
-          </Typography> */}
-
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <Link
-              underline='none'
-              href='#'
-              className={classes.link}
-              color='inherit'>
-              Request a Boon
+          <Link
+            underline='none'
+            href='#'
+            className={classes.link}
+            color='inherit'>
+            My Boons
+          </Link>
+          <Link
+            underline='none'
+            href='#'
+            className={classes.link}
+            color='inherit'>
+            Explore
+          </Link>)} */}
+              <IconButton
+                edge='end'
+                aria-label='account of current user'
+                aria-controls={menuId}
+                aria-haspopup='true'
+                onClick={handleProfileMenuOpen}
+                color='inherit'>
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label='show more'
+                aria-controls={mobileMenuId}
+                aria-haspopup='true'
+                onClick={handleMobileMenuOpen}
+                color='inherit'>
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.grow}>
+        <AppBar className={classes.appBar} position='static'>
+          <Toolbar>
+            <Link underline='none' href='/' color='primary'>
+              <img
+                src='./images/Boon-big.svg'
+                alt='Boon-logo'
+                className={classes.logo}
+              />
             </Link>
-            <Link
-              underline='none'
-              href='#'
-              className={classes.link}
-              color='inherit'>
-              My Boons
-            </Link>
-            <Link
-              underline='none'
-              href='#'
-              className={classes.link}
-              color='inherit'>
-              Explore
-            </Link>
-            <IconButton
-              edge='end'
-              aria-label='account of current user'
-              aria-controls={menuId}
-              aria-haspopup='true'
-              onClick={handleProfileMenuOpen}
-              color='inherit'>
-              <AccountCircle />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label='show more'
-              aria-controls={mobileMenuId}
-              aria-haspopup='true'
-              onClick={handleMobileMenuOpen}
-              color='inherit'>
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-  );
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              <Link
+                underline='none'
+                href='#'
+                className={classes.link}
+                color='inherit'>
+                Request a Boon
+              </Link>
+              <Link
+                underline='none'
+                href='#'
+                className={classes.link}
+                color='inherit'>
+                My Boons
+              </Link>
+              <Link
+                underline='none'
+                href='#'
+                className={classes.link}
+                color='inherit'>
+                Explore
+              </Link>
+              <IconButton
+                edge='end'
+                aria-label='account of current user'
+                aria-controls={menuId}
+                aria-haspopup='true'
+                onClick={handleProfileMenuOpen}
+                color='inherit'>
+                <AccountCircle />
+              </IconButton>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label='show more'
+                aria-controls={mobileMenuId}
+                aria-haspopup='true'
+                onClick={handleMobileMenuOpen}
+                color='inherit'>
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {renderMobileMenu}
+        {renderMenu}
+      </div>
+    );
+  }
 }
