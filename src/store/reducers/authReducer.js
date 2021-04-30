f1048ed8a232aac92aede9e006be9407c7186ca9
Azael Alonso Campana <zarzarius@gmail.com>
2021-04-30 12:16:11 +0200

    debugging login and navbar private contents

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,14 +1,14 @@
 import { USER_LOGIN, USER_LOGOUT } from "../actions/types";
 const initialState = {
   user: {
-    // avatar: "/statics/avatar.png",
-    // _id: "608a7b96df0b0817bc45336d",
-    // firstName: "trehdfgdgft",
-    // lastName: "hfgffdsf",
-    // userName: "newusernam123",
-    // email: "email2222@gmail.com",
-    // birthday: "2020-10-23T00:00:00.000Z",
-    // password: "Josiecat1234!",
+    avatar: "/statics/avatar.png",
+    _id: "608a7b96df0b0817bc45336d",
+    firstName: "trehdfgdgft",
+    lastName: "hfgffdsf",
+    userName: "newusernam123",
+    email: "email2222@gmail.com",
+    birthday: "2020-10-23T00:00:00.000Z",
+    password: "Josiecat1234!",
   },
 };
 
