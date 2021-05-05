a9182db34e27319c2ad85fe2bb3692c6daa44736
Cindy <Punk.c@hotmail.com>
2021-05-05 11:35:50 +0200

    add last changes

--- src/store/reducers/errorReducer.js
+++ src/store/reducers/errorReducer.js
@@ -1,23 +1,23 @@
 // let's see if this works
-import { HIDE_ERROR, SET_ERROR } from '../actions/types';
+import { HIDE_ERROR, SET_ERROR } from "../actions/types";
 
 const initState = {
-    error: null,
-    isOpen: false
-   };
+	error: null,
+	isOpen: false,
+};
 
-export function errorReducer(state = initState, action){
-    switch(action.type){
-        case SET_ERROR:
-         return {
-            error: action.payload,
-            isOpen: true
-        } 
-        case HIDE_ERROR:
-            return initState 
-        default:
-            return state;
-    }
-
+export function errorReducer(state = initState, action) {
+	console.log(action.payload);
+	switch (action.type) {
+		case SET_ERROR:
+			return {
+				error: action.payload,
+				isOpen: true,
+			};
+		case HIDE_ERROR:
+			return initState;
+		default:
+			return state;
+	}
 }
 export default errorReducer;
