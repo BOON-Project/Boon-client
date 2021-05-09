fde5b159c87060845589fd55d2a53679763ef5ec
Cindy <Punk.c@hotmail.com>
2021-05-09 21:31:31 +0200

    add renderSkillItems function but not finished

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,13 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 //MUI styling =>
-import avatar from "./img/avatar.jpg";
 import {
-  Box,Typography,Avatar,List,ListItem,ListItemText,ListItemAvatar,ListItemSecondaryAction,Paper,Divider,Container,CssBaseline,Grid,Button,} from "@material-ui/core";
-import LinkedCameraIcon from "@material-ui/icons/LinkedCamera";
-import StarIcon from "@material-ui/icons/Star";
-import AddIcon from "@material-ui/icons/Add";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+  Box,Typography,List,ListItem,ListItemText,ListItemSecondaryAction,Paper,Divider,CssBaseline,Grid,Button, ButtonGroup, Link,} from "@material-ui/core";
 import PersonPinIcon from "@material-ui/icons/PersonPin";
 import Rating from '@material-ui/lab/Rating';
 
@@ -15,7 +10,6 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from "./styles";
 import { useDispatch, useSelector, Redirect } from "react-redux";
 import { useHistory } from "react-router";
-import axios from "axios";
 import { getUsersAction } from "../../store/actions/usersAction";
 import {getSkillsAction } from "../../store/actions/skillsActions"
 import {getTasksAction} from "../../store/actions/tasksActions"
@@ -32,10 +26,46 @@ export default function UserProfile(props){
   // to go back
   const history = useHistory();
 
-  //deconstruct user info
+  //deconstruct user's info
   const {
     firstName, userName,bio,rating,avatar,skills
   } = props.location.state.user;
+
+
+  //Rendering skills!
+// const skillItems = useSelector((state) => state.skills);
+
+// const renderSkillsItems = skillItems.map((skill, index)=> (
+// <div className={classes.root}>
+//   <Grid container spacing={3}>
+
+//     {/* Header */}
+//     <Grid item xs={12} sm={6}>
+//       <Paper className={classes.paper}>Offering</Paper>
+//     </Grid>
+
+//     <Grid item xs={6} sm={3}>
+//           <Paper className={classes.paper}>Skill</Paper>
+//     </Grid>
+//         <Grid item xs={6} sm={3}>
+//           <Paper className={classes.paper}>Boons</Paper>
+//         </Grid>
+
+//       {/* Skills List */}
+//       <div key={index} className={classes.root}>
+
+//       <Grid item xs={6} sm={3}>
+//           <Paper className={classes.paper}>{skill.name}</Paper>
+//     </Grid>
+//         <Grid item xs={6} sm={3}>
+//           <Paper className={classes.paper}>{skill.boons}</Paper>
+//         </Grid>
+//       </div>
+
+//   </Grid>
+// </div>
+// ))
+
 
   //Rendering part! =>
   return (
@@ -133,6 +163,43 @@ export default function UserProfile(props){
           </Grid>
         </Grid>
       </div>
+
+
+
+      {/* Last 2 Buttons!  */}
+
+      <Box className={classes.root} textAlign='center'>
+
+            <ButtonGroup disableElevation variant="contained"   >
+
+              <Button
+              component={Link} to='/home'
+              size='large'
+              color='secondary'
+              variant='contained'
+              onClick={() => history.goBack()}
+              className={classes.button}>
+              Go back
+              </Button>
+
+              <Button
+              component={Link} to='/home'
+              size='large'
+              color='secondary'
+              variant='contained'
+
+              //pending!!
+              onClick={() => history.goBack()}
+              className={classes.button}>
+              Send a message
+              </Button>
+          </ButtonGroup>
+
+
+
+
+
+      </Box>
     </CssBaseline>
   );
 };
