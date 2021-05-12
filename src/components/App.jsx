34d729b966c84aebc17e236b8f9f6cd2e1681d72
Cindy Giron <35552219+mizzrawr@users.noreply.github.com>
2021-05-12 14:26:06 +0200

    fix app js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,34 +13,7 @@ import Error404 from "./Error404";
 import RequestBoon from "./RequestBoon/RequestBoon";
 
 const App = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 98a4d8a03f9957f91ab2eeab17ffb39d8d6db92c
-  return (
-    <>
-      <Nav />
-      <Switch>
-        {/* new files structure */}
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Signup" component={Signup} />
-        <Route exact path="/Login" component={Login} />
-        <Route exact path="/UserProfile/:id" component={UserProfile} />
-<<<<<<< HEAD
-        <Route exact path="/skills" component={SkillByUser} />
-=======
-        <Route exact path="/RequestBoon/:id" component={RequestBoon} />
-        <Route exact path="/skill/:id" component={SkillByUser} />
->>>>>>> 98a4d8a03f9957f91ab2eeab17ffb39d8d6db92c
-        {/* Private Routes =>  */}
-        <PrivateRoute path="/EditUser" component={EditUser} />
-        {/* Error 404 Route =>  */}
-        <Route path="/*" component={Error404} />
-      </Switch>
-    </>
-  );
-<<<<<<< HEAD
-=======
+
 	return (
 		<>
 			<Nav />
@@ -58,9 +31,7 @@ const App = () => {
 			</Switch>
 		</>
 	);
->>>>>>> 1cb3f43c88cab848e2b48c5a2026fdc89853a759
-=======
->>>>>>> 98a4d8a03f9957f91ab2eeab17ffb39d8d6db92c
+
 };
 
 export default App;
