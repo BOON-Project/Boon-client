79f2afe05bf8da1453150ceb95be2767b38f160c
Enikő Veszelka <encsooo@gmail.com>
2021-05-19 15:48:05 +0200

    about us

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,34 +1,45 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
-
-//styling=>
-import {
-  Avatar,
-  Typography,
-  Box,
-  Container,
-  Button,
-  Grid,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Paper,
-} from "@material-ui/core";
-import Footer from '../Footer/Footer';
+import { TextField, Box, Typography } from "@material-ui/core";
 
 export default function AboutUs() {
   const classes = useStyles();
 
   return (
     <>
-
-
-
-
-
-      <Footer />
+      <Box pt="2rem">
+        <Typography variant="h6" color="info">
+          Do you have any questions?
+        </Typography>
+      </Box>
+      <TextField
+        id="outlined-textarea"
+        label="Your name"
+        placeholder="Placeholder"
+        multiline
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-textarea"
+        label="Your email"
+        placeholder="Placeholder"
+        multiline
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-textarea"
+        label="Subject"
+        placeholder="Placeholder"
+        multiline
+        variant="outlined"
+      />
+      <TextField
+        id="outlined-textarea"
+        label="Your message"
+        placeholder="Placeholder"
+        multiline
+        variant="outlined"
+      />
     </>
   );
 }
