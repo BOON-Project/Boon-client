f0caa09664c1f56a7c351d1bb6fe43a1a27fa677
Cindy Giron <35552219+mizzrawr@users.noreply.github.com>
2021-05-19 20:19:16 +0200

    style aboutus & add ext links

--- src/components/AboutUs/styles.js
+++ src/components/AboutUs/styles.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import team from "../../images/people-discussion-5069845_1920.jpg";
 import berlin from "../../images/architecture-3171308_1920.jpg";
 
+
 export default makeStyles((theme) => ({
   main: {
     width: "60%",
@@ -37,5 +38,31 @@ export default makeStyles((theme) => ({
     backgroundImage: `url(${berlin})`,
     height: "400px",
     width: "100%",
+
   },
+  lastCard:{
+    display: 'flex',
+
+    backgroundImage: `url(https://images.unsplash.com/photo-1516149893016-813d9a01d5d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80)`,
+    height: "500px",
+    backgroundPosition: "bottom",
+    backgroundRepeat: 'no-repeat',
+    marginBottom: "1rem"
+
+  },
+  insideText:{
+    padding: theme.spacing(3, 2),
+    height: 465,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+icons:{
+  justifyContent: 'center'
+},
+icon: {
+  fontSize: '4rem',
+  color: 'white',
+}
+
 }));
