0464ef13da83d7a2e3f4a6207b0fc1c2f3904557
Cindy Giron <35552219+mizzrawr@users.noreply.github.com>
2021-05-19 14:24:34 +0200

    add contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
+
+//styling=>
+import {
+  Avatar,
+  Typography,
+  Box,
+  Container,
+  Button,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Paper,
+} from "@material-ui/core";
+import Footer from '../Footer/Footer';
+
+export default function AboutUs() {
+  const classes = useStyles();
+
+  return (
+    <>
+
+
+
+
+
+      <Footer />
+    </>
+  );
+}
