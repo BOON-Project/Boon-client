2170ca0e815b26a9afc7caef523bed4625108e01
Enikő Veszelka <encsooo@gmail.com>
2021-05-20 15:02:22 +0200

    contact update

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,14 +1,32 @@
 import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   root: {
+    display: "flex",
+    justifyContent: "center",
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: "25ch",
     },
+    // "& .MuiGrid-item": {
+    //   padding: 0,
+    // },
+  },
+
+  contact: {
+    backgroundColor: "#E4B363",
+
+    //height: "400px",
   },
   wrapper: {
-    width: "300px",
+    width: "auto",
     display: "flex",
+    backgroundColor: "#E4B363",
     flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "380px",
+  },
+  video: {
+    height: "380px",
   },
 }));
