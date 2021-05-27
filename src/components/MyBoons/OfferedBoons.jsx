d8b7c5157adf432b5d367cdf0466c042e4770ab1
Azael Alonso Campana <zarzarius@gmail.com>
2021-05-27 13:22:50 +0200

    tasksOffered in progress

--- src/components/MyBoons/OfferedBoons.jsx
+++ src/components/MyBoons/OfferedBoons.jsx
@@ -1,3 +1,4 @@
+import { Box } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserOfferedTasksAction } from "../../store/actions/tasksActions";
@@ -15,7 +16,18 @@ const RequestedBoons = () => {
     return (
         <>
             {tasks.map((task) => (
-                <p>{task._id}</p>
+                <Box key={task._id} style={{ border: "1px solid" }}>
+                    <span>{task.status}</span>
+                    <br />
+                    <span>{task.boonee.userName}</span>
+                    <br />
+                    <span>{task.skill.name}</span>
+                    <br />
+                    <span>{task.boons}</span>
+                    <br />
+                    <span>{task.message}</span>
+                    <br />
+                </Box>
             ))}
         </>
     );
