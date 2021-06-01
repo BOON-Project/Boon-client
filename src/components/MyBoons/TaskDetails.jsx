53746bdebd810e8902d09ff3a117e2dacb77f48d
Enikő Veszelka <encsooo@gmail.com>
2021-06-01 09:32:39 +0200

    save

--- src/components/MyBoons/TaskDetails.jsx
+++ src/components/MyBoons/TaskDetails.jsx
@@ -1,14 +1,14 @@
 import {
-    Paper,
-    Typography,
-    Box,
-    Card,
-    Button,
-    Grid,
-    Avatar,
-    CardContent,
-    Container,
-    Chip,
+  Paper,
+  Typography,
+  Box,
+  Card,
+  Button,
+  Grid,
+  Avatar,
+  CardContent,
+  Container,
+  Chip,
 } from "@material-ui/core";
 import React from "react";
 import { useSelector } from "react-redux";
@@ -17,110 +17,118 @@ import useStyles from "./styles";
 const allImages = require.context("../../images", true, /.jpg$/);
 
 const TaskDetails = (props) => {
-    const user = useSelector((state) => state.userReducer.user);
-    const history = useHistory();
-    const classes = useStyles();
-    const task = props.location.state.task;
+  const user = useSelector((state) => state.userReducer.user);
+  const history = useHistory();
+  const classes = useStyles();
+  const task = props.location.state.task;
 
-    //console.log(task.boonee.firstName);
-    return (
-        <>
-            <Container maxWidth='md' className={classes.root}>
-                <Paper className={classes.paperTaskDetails}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={9}>
-                            <Grid item xs={12}>
-                                {/* avatar and small avatar (badge)*/}
-                                {user._id !== task.boonee._id ? (
-                                    <Avatar
-                                        className={classes.avatar}
-                                        alt='boonee avatar'
-                                        src={task.boonee.avatar}></Avatar>
-                                ) : (
-                                    <Avatar
-                                        className={classes.avatar}
-                                        alt='booner avatar'
-                                        src={task.booner.avatar}></Avatar>
-                                )}
+  //console.log(task.boonee.firstName);
+  return (
+    <>
+      <Container maxWidth="md" className={classes.root}>
+        <Paper className={classes.paperTaskDetails}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Grid item xs={12} className={classes.avatarWrap}>
+                <Grid container spacing={3}>
+                  <Grid item xs={6} md={3}>
+                    {/* avatar and small avatar (badge)*/}
+                    {user._id !== task.boonee._id ? (
+                      <Avatar
+                        className={classes.avatar}
+                        alt="boonee avatar"
+                        src={task.boonee.avatar}
+                      ></Avatar>
+                    ) : (
+                      <Avatar
+                        className={classes.avatar}
+                        alt="booner avatar"
+                        src={task.booner.avatar}
+                      ></Avatar>
+                    )}
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>
+                      {user._id !== task.boonee._id ? (
+                        <b>Boonee: {task.boonee.userName}</b>
+                      ) : (
+                        <b>Booner: {task.booner.userName}</b>
+                      )}
+                    </Typography>
 
-                                <Chip
-                                    label={task.skill.name}
-                                    variant='outlined'
-                                    color='info'
-                                    className={classes.tag}>
-                                    {task.skill.name}
-                                </Chip>
-                            </Grid>
-                            {/* NAMES RATING AND SKILL CONTAINER */}
-                            <Typography>
-                                {user._id !== task.boonee._id ? (
-                                    <b>Boonee: {task.boonee.userName}</b>
-                                ) : (
-                                    <b>Booner: {task.booner.userName}</b>
-                                )}
-                            </Typography>
+                    {/* date  */}
+                    <Typography>Date: {task.date.slice(0, 10)}</Typography>
 
-                            {/* date  */}
-                            <Typography>Date: {task.date}</Typography>
+                    <Typography>Task code: {task._id}</Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Chip
+                label={task.skill.name}
+                variant="outlined"
+                color="info"
+                className={classes.tag}
+              >
+                {task.skill.name}
+              </Chip>
+            </Grid>
+            {/* NAMES RATING AND SKILL CONTAINER */}
 
-                            <Typography>Task code: {task._id}</Typography>
-                        </Grid>
+            <Grid item xs={12}>
+              <Typography>Message:</Typography>
+            </Grid>
 
-                        <Grid item xs={12}>
-                            <Typography>Message:</Typography>
-                        </Grid>
+            <Grid item xs={8}>
+              <Typography variant="body1" p={4}>
+                {task.message}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {" "}
+              <img
+                alt="img"
+                src={allImages(`./${task.skill.avatar}`).default}
+                className={classes.taskimg}
+              ></img>
+            </Grid>
 
-                        <Grid item xs={8}>
-                            <Typography variant='body1' p={4}>
-                                {task.message}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            {" "}
-                            <img
-                                alt='img'
-                                src={
-                                    allImages(`./${task.skill.avatar}`).default
-                                }
-                                className={classes.taskimg}></img>
-                        </Grid>
+            {/* SKILL */}
 
-                        {/* SKILL */}
+            {/* RATING TEXT */}
+            <Typography variant="body1" p={4}>
+              {task.status}
+            </Typography>
 
-                        {/* RATING TEXT */}
-                        <Typography variant='body1' p={4}>
-                            {task.status}
-                        </Typography>
-
-                        <Grid container>
-                            <Grid item xs={4}>
-                                asfwef
-                            </Grid>
-                            <Grid item xs={4}>
-                                awefwe
-                            </Grid>
-                            <Grid item xs={4}>
-                                awfwef
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Paper>
-                {/* Last last button at bottom */}
-            </Container>
-            <Box className={classes.buttonBox}>
-                <Button
-                    to='/home'
-                    size='large'
-                    //you can change this color as you like im only testing
-                    color='primary'
-                    variant='contained'
-                    onClick={() => history.goBack()}
-                    className={classes.button}>
-                    Go back
-                </Button>
-            </Box>
-        </>
-    );
+            <Grid container>
+              <Grid item xs={4}>
+                asfwef
+              </Grid>
+              <Grid item xs={4}>
+                awefwe
+              </Grid>
+              <Grid item xs={4}>
+                awfwef
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+        {/* Last last button at bottom */}
+      </Container>
+      <Box className={classes.buttonBox}>
+        <Button
+          to="/home"
+          size="large"
+          //you can change this color as you like im only testing
+          color="primary"
+          variant="contained"
+          onClick={() => history.goBack()}
+          className={classes.button}
+        >
+          Go back
+        </Button>
+      </Box>
+    </>
+  );
 };
 
 export default TaskDetails;
--- src/components/MyBoons/styles.js
+++ src/components/MyBoons/styles.js
@@ -1,75 +1,80 @@
 import { makeStyles } from "@material-ui/core";
 
 export default makeStyles((theme) => ({
-    root: {
-        display: "flex",
-    },
-    paper: {
-        width: "100%",
-        marginTop: theme.spacing(4),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "1rem",
-        height: "75vh",
-        overflow: "scroll",
-    },
-    paperTaskDetails: {
-        width: "100%",
-        marginTop: theme.spacing(4),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "1rem",
-        height: "75vh",
-    },
-    box: {
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        height: "45px",
-        backgroundColor: "#E4B363",
-    },
-    buttonBox: {
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "2rem",
-    },
-    ratingCard: {
-        // background: "hsla(37, 70%, 64%, 1)",
-    },
-    image: {
-        width: "80%",
-        height: "80%",
-        border: "4px solid #329282",
-        borderRadius: "10px",
-    },
-    avatar: {
-        width: "100px",
-        height: "100px",
-        border: "4px solid #329282",
-    },
-    avatarwrap: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-    },
-    // taskDetails
-    taskimg: {
-        height: "200px",
-    },
-    //offeredboons
-    gridinfo: {
-        paddingLeft: "1rem",
-    },
-    tag: {
-        color: "black",
-    },
-    boxImage: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        alignSelf: "center",
-    },
+  root: {
+    display: "flex",
+  },
+  paper: {
+    width: "100%",
+    marginTop: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "1rem",
+    height: "75vh",
+    overflow: "scroll",
+  },
+  paperTaskDetails: {
+    width: "100%",
+    marginTop: theme.spacing(4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "1rem",
+    height: "75vh",
+  },
+  box: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "45px",
+    backgroundColor: "#E4B363",
+  },
+  buttonBox: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "2rem",
+  },
+  ratingCard: {
+    // background: "hsla(37, 70%, 64%, 1)",
+  },
+  image: {
+    width: "80%",
+    height: "80%",
+    border: "4px solid #329282",
+    borderRadius: "10px",
+  },
+  avatar: {
+    width: "100px",
+    height: "100px",
+    border: "4px solid #329282",
+  },
+  avatarwrap: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  // taskDetails
+  taskimg: {
+    height: "200px",
+  },
+  avatarWrap: {
+    backgroundColor: "rgb(50,144,143, 0.2)",
+    borderRadius: "10px",
+    padding: "10px",
+  },
+  //offeredboons
+  gridinfo: {
+    paddingLeft: "1rem",
+  },
+  tag: {
+    color: "black",
+  },
+  boxImage: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
 }));
